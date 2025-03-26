a837f49b496af3bc02dc4cfd5faf644e36957ccd
Rachel H Barcellona <rbarcellona@tampabay.rr.com>
2025-03-26 13:08:51 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,27 +1,20 @@
-const mongoose = require('mongoose');
+const Todo = require('./models/todo.js');
 
-const customerSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
-const customer = mongoose.model ('Customer', customerSchema)
-module.exports = customer 
-//const displayMenu = () => {
-    //console.log('Welcome to the Customer Management System');
-   // console.log('1. Create Customer');
-    //console.log('2. View Customers');
-    //console.log('3. Update Customer');
-    //console.log('4. Delete Customer');
-    //console.log('5. Quit');
-    //const choice = prompt('Choose an option (1-5): ');
-  
-    //return choice;
-  //};
+const createTodo = async () => {
 
-//const prompt = require('prompt-sync')();
+    const todoData = {
+      name: "String",
+      age: Number,
+    };
+    
+    const todo = await Todo.create(todoData);
+    console.log("New todo:", todo);
+  };
 
-//const greeting = prompt('Welcome');
-//onsole.log(`Welcome ${username}`);
+const runQueries = async () => {
+    console.log('Queries running.');
+    await createTodo();
+  };
 
   
   
